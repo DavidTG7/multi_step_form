79570a37db8f58d6353129e3dd0f7d019d95f975
DavidTG7 <davidthekoa@gmail.com>
2023-02-11 18:46:36 -0500

    Create GoBackButton component

---
+++ src/components/goBackButton/GoBackButton.jsx
@@ -0,0 +1,5 @@
+import { GoBackBackAnchor } from "./goBackButton";
+
+export const GoBackButton = () => {
+  return <GoBackBackAnchor href="#">Go Back</GoBackBackAnchor>;
+};
---
+++ src/components/goBackButton/goBackButton.js
@@ -0,0 +1,15 @@
+import styled from "styled-components";
+
+export const GoBackBackAnchor = styled.a`
+  position: absolute;
+  bottom: 3.6rem;
+  text-decoration: none;
+  color: var(--cool-gray);
+  font-size: 1.7rem;
+  font-weight: 700;
+  transition: .2s;
+
+  &:hover {
+    color: var(--marine-blue);
+  }
+`

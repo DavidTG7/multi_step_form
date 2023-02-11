cbda372e4ea0e26ef49e8a782daf28522425a768
DavidTG7 <davidthekoa@gmail.com>
2023-02-11 15:57:27 -0500

    Create PageContainer as a componente to reuse it in each page

---
+++ src/components/pageContainer/PageContainer.jsx
@@ -0,0 +1,5 @@
+import { PageContainerBox } from "./pageContainer";
+
+export const PageContainer = ({children}) => {
+  return <PageContainerBox>{children}</PageContainerBox>;
+};
---
+++ src/components/pageContainer/pageContainer.js
@@ -0,0 +1,9 @@
+import styled from "styled-components";
+
+export const PageContainerBox = styled.div`
+  position: relative;
+  display: flex;
+  flex-flow: column;
+  padding: 4rem 7rem;
+  width: 45rem;
+`;
